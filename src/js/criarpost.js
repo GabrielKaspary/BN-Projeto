e66550ff623cb9d5f9c1ae797fe8477b13d087ce
gabrielaltenhofen <ga0125419@setrem.com.br>
2023-10-22 16:48:55 -0300

    adicionada função criar post

--- src/js/criarpost.js
+++ src/js/criarpost.js
@@ -1,49 +1,64 @@
-
-
-const database = app.database();
+// Firebase Realtime Database
+const database = firebase.database();
 const postsRef = database.ref('posts');
 
-// Referência ao Firebase Storage
-const storage = getStorage(app);
+// Firebase Storage
+const storage = firebase.storage();
 
 // Manipular o envio do formulário
-const form = document.getElementById('post-form');
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById('post-form');
+    
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-    const titulo = document.getElementById('titulo').value;
-    const cidade = document.getElementById('cidade').value;
-    const hora = document.getElementById('hora').value;
-    const descricao = document.getElementById('descricao').value;
-    const imagensInput = document.getElementById('imagens');
+        const titulo = document.getElementById('titulo').value;
+        const cidade = document.getElementById('cidade').value;
+        const hora = document.getElementById('hora').value;
+        const descricao = document.getElementById('descricao').value;
+        const imagensInput = document.getElementById('imagens');
 
-    // Realizar upload das imagens para o Firebase Storage
-    const imagensFiles = imagensInput.files;
-    const imagensUrls = [];
+        // Realizar upload das imagens para o Firebase Storage
+        const imagensFiles = imagensInput.files;
+        const imagensUrls = [];
 
-    for (let i = 0; i < imagensFiles.length; i++) {
-        const imagemFile = imagensFiles[i];
-        const imagemRef = ref(storage, 'imagens/' + imagemFile.name);
+        const promises = [];
 
-        await uploadBytes(imagemRef, imagemFile).then(async (snapshot) => {
-            // Obter a URL da imagem após o upload
-            const url = await getDownloadURL(imagemRef);
-            imagensUrls.push(url);
+        for (let i = 0; i < imagensFiles.length; i++) {
+            const imagemFile = imagensFiles[i];
+            const imagemRef = storage.ref('imagens/' + imagemFile.name);
 
-            // Verificar se todas as imagens foram processadas
-            if (imagensUrls.length === imagensFiles.length) {
-                // Salvar os dados, incluindo as URLs das imagens, no Firebase Realtime Database
-                postsRef.push({
-                    titulo: titulo,
-                    cidade: cidade,
-                    hora: hora,
-                    descricao: descricao,
-                    imagens: imagensUrls
+            const uploadTask = imagemRef.put(imagemFile);
+
+            // Capturar a URL da imagem após o upload
+            const urlPromise = new Promise((resolve, reject) => {
+                uploadTask.on('state_changed', null, null, () => {
+                    uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                        imagensUrls.push(downloadURL);
+
+                        if (imagensUrls.length === imagensFiles.length) {
+                            // Salvar os dados, incluindo as URLs das imagens, no Firebase Realtime Database
+                            postsRef.push({
+                                titulo: titulo,
+                                cidade: cidade,
+                                hora: hora,
+                                descricao: descricao,
+                                imagens: imagensUrls
+                            });
+
+                            // Limpar o formulário após o envio
+                            form.reset();
+                        }
+                        resolve();
+                    });
                 });
+            });
 
-                // Limpar o formulário após o envio
-                form.reset();
-            }
+            promises.push(urlPromise);
+        }
+
+        Promise.all(promises).then(() => {
+            // Todas as URLs foram obtidas
         });
-    }
+    });
 });
